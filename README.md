c621513306096981471b462930e1d2ed5efea811
SeiichiN <billie175@gmail.com>
2021-03-03 21:59:18 +0900

    関数にしたから、わかりやすくなった。

--- README.md
+++ README.md
@@ -6,7 +6,13 @@ Emacs-Lispの練習
 
 勉強用においておく。
 
+
 ## 使いかた
+
+C-x C-f で attack.el を読み込む。
+M-x eval-buffer で、attack.el を評価する。
 
 適当にバッファを開いて、そこで、M-x attack とする。
 
+
+<!-- 修正時刻: Wed Mar  3 21:58:51 2021 -->
--- attack.el
+++ attack.el
@@ -5,15 +5,34 @@
 
 (defun attack ()
   (interactive)
-  (let ((enemy 100) damage )
+  (let ((monster 100)    ; monster -- モンスターのライフポイント
+        (hero 100)       ; hero -- 勇者のライフポイント
+        monster-damage
+        hero-damage)
     (insert "モンスターが現れた。勇者は戦いを挑んだ。\n")
     (insert "戦闘スタート!\n")
-    (while (> enemy 0)
-      (setq damage (random 30))
-      (setq enemy (- enemy damage))
-      (if (< enemy 0)
-          (insert "勇者はモンスターを倒した\n")
-        (insert (format "モンスターに %d のダメージを与えた\n" enemy) )))))
+    (while (and (> monster 0) (> hero 0))
+      (setq monster (attack-enemy "勇者" "モンスター" monster))
+      (if (> monster 0)
+          (setq hero (attack-enemy "モンスター" "勇者" hero)))
+      (if (< monster 1)
+          (insert "勇者はモンスターを倒した\n"))
+      (if (< hero 1)
+          (insert "勇者はモンスターにやられてしまった\n")))))
 
+;; @param
+;;   my -- String 自分
+;;   enemy -- String 攻撃対象
+;;   life-point -- int 攻撃対象のライフポイント
+;; @return
+;;   life-point -- int ダメージを減算したあとのライフポイント
+;;
+(defun attack-enemy (my enemy life-point)
+  (setq damage (random 30))
+  (setq life-point (- life-point damage))
+  (insert (format "%s の攻撃!\n" my))
+  (insert (format "%s は %d のダメージを負った。hpは %d になった。\n"
+                  enemy damage life-point))
+  life-point)
 
-;; 修正時刻: Wed Mar  3 16:26:10 2021
+;; 修正時刻: Wed Mar  3 21:52:43 2021
